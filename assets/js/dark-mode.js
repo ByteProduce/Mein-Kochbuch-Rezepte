ad2c05503f6915baa9a26b81148c9cd604427a45
ByteProduce <53964587+ByteProduce@users.noreply.github.com>
2025-03-24 16:52:30 +0100

    Delete assets directory

--- assets/js/dark-mode.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($) {
-    $('#dark-mode-toggle').click(function() {
-        $('body').toggleClass('dark-mode');
-        localStorage.setItem('darkMode', $('body').hasClass('dark-mode') ? 'enabled' : 'disabled');
-    });
-    if (localStorage.getItem('darkMode') === 'enabled') {
-        $('body').addClass('dark-mode');
-    }
-});
